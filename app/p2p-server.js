8d56a0b5b541feaeed1de9e5b889f290e0b9fb19
Jerry Gerou <jerrysgerou@gmail.com>
2018-09-02 10:21:15 -0500

    Fix typos, get peer-to-peer servers working

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -14,7 +14,21 @@ const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers =process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 // Combines all peers environment variables with string
-// $ HHTP_PORT=3002 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
+// This will define a new port, a peer port and the peer location already open
+// HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
+
+// [nodemon] starting `node ./app`
+// Listening for peer-to-perr connections on: 5002
+// Listening on port 3002
+// Socket connected. Socket to me.
+
+// Can then open a new tab and create another peer
+// HTTP_PORT=3003 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
+// [nodemon] starting `node ./app`
+// Listening for peer-to-perr connections on: 5003
+// Listening on port 3003
+// Socket connected. Socket to me.
+// Socket connected. Socket to me.
 
 class P2pServer {
   constructor(blockchain) {
@@ -24,10 +38,25 @@ class P2pServer {
 
   // listen() starts up server and creates Websocket
   listen() {
-    const server = new.Websocket.Server({ port: P2P_PORT });
+    const server = new Websocket.Server({ port: P2P_PORT });
     // listening for a connection event
-    server.on('connection', server => this.connectSocket(socket));
+    server.on('connection', socket => this.connectSocket(socket));
+
+    this.connectToPeers();
+
     console.log(`Listening for peer-to-perr connections on: ${P2P_PORT}`);
+  }
+
+  connectToPeers() {
+    peers.forEach(peer => {
+      // Address of peer
+      // ws://localhost:5001
+      // creates new socket object
+      const socket = new Websocket(peer);
+
+      // Can run even if someone else is on first
+      socket.on('open', () => this.connectSocket(socket));
+    });
   }
 
   // pushes socket ot the array of sockets
@@ -36,3 +65,5 @@ class P2pServer {
     console.log('Socket connected. Socket to me.');
   }
 }
+
+module.exports = P2pServer;
