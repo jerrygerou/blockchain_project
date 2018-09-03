dcdec24c6c3cf6c61a58dcfa8d01daad44879168
Jerry Gerou <jerrysgerou@gmail.com>
2018-09-03 10:04:05 -0500

    Create wallet with a blance and a public key

--- wallet/index.js
+++ wallet/index.js
@@ -1,11 +1,13 @@
-class { INITIAL_BALANCE } = require('../config');
+const ChainUtil = require('../chain-util');
+const { INITIAL_BALANCE } = require('../config');
 
 class Wallet {
   constructor() {
     // for the sake of testing, give an itial balance
     this.balance = INITIAL_BALANCE;
-    this.keyPair = null;
-    this.publicKey = null;
+    this.keyPair = ChainUtil.genKeyPair();
+    // ChainUtil creates the public key, then encode with hexi-decimal
+    this.publicKey = this.keyPair.getPublic().encode('hex');
   }
 
   toString() {
