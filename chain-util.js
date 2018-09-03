cca314f43e78ff447cf3d05e3fedca21e51222e3
Jerry Gerou <jerrysgerou@gmail.com>
2018-09-03 11:11:17 -0500

    Add test for adding signature to transaction

--- chain-util.js
+++ chain-util.js
@@ -1,5 +1,6 @@
 // Elliptic cryptography
 const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
 // Ability to gernate unique IDs for transactions
 const uuidV1 = require('uuid/v1');
 // Creates instance using implementation bitcoin uses, secp256k1
@@ -9,8 +10,13 @@ class ChainUtil {
   static genKeyPair() {
     return ec.genKeyPair();
   }
+
   static id() {
     return uuidV1();
+  }
+
+  static hash(data) {
+    return SHA256(JSON.stringify(data)).toString();
   }
 }
 
