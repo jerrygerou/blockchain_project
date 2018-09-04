4c5f9c0e5628471171af4e20649d97d6cb718b82
Jerry Gerou <jerrysgerou@gmail.com>
2018-09-04 18:53:53 -0500

    Test for adding or updating from transaction pool

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -10,5 +10,18 @@ describe('TransactionPool', () => {
     wallet = new Wallet();
     transaction = Transaction.newTransaction(wallet, 'r4nd-4dr355', 30);
     tp.updateOrAddTransaction(transaction);
-  })
-})
+  });
+
+  it('adds a transaction to the pool', () => {
+    expect(tp.transactions.find(t => t.id == transaction.id)).toEqual(transaction);
+  });
+
+  it('updates a transaction in the pool', () => {
+    const oldTransaction = JSON.stringify(transaction);
+    const newTransaction = transaction.update(wallet, 'foo-4ddr355', 40);
+    tp.updateOrAddTransaction(newTransaction);
+
+    expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id)))
+      .not.toEqual(oldTransaction);
+  });
+});
